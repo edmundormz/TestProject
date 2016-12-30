0351261c054ab91d2729fd2e712e6287b0eed034
H. Edmundo Ramírez <edmundormz@gmail.com>
2016-12-30 10:04:07 -0600

    Added Custom Controller

---
+++ TestProject/TestProject.WebApp/Controllers/ToyController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestProject.Core;
+using TestProject.Infrastructure;
+
+namespace TestProject.WebApp.Controllers
+{
+    public class ToyController : ApiController
+    {
+        ToyRepository repo; 
+
+        // GET api/<controller>
+        public List<Toy> Get()
+        {
+            repo = new ToyRepository();
+            var toys = repo.GetToys();
+            return toys;
+        }
+
+        // GET api/<controller>/5
+        public Toy GetById(int Id)
+        {
+            repo = new ToyRepository();
+            var foundToy = repo.FindById(Id);
+            return foundToy;
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]Toy newToy)
+        {
+            repo = new ToyRepository();
+            repo.Add(newToy);
+        }
+
+        // PUT api/<controller>/5
+        public void Put([FromBody]Toy editedToy)
+        {
+            repo = new ToyRepository();
+            repo.Edit(editedToy);
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int Id)
+        {
+            repo = new ToyRepository();
+            repo.Remove(Id);
+        }
+    }
+}
---
+++ TestProject/TestProject.WebApp/app.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app', []).controller(function($scope) {
+        
+        $scope.listaProyectos = [
+           { nombre: "A", lenguaje: "Visual C#", tipo: "Web", horas: 1000 },
+           { nombre: "B", lenguaje: "Visual Basic", tipo: "Mobile", horas: 250 },
+           { nombre: "C", lenguaje: "TypeScript", tipo: "Web", horas: 2000 }];
+
+    })});
